f6abf3594698c81412c9110d02820df7eadae782
Lakshya Asati <lakshyaasati176@gmail.com>
2026-09-13 23:07:49 +0530

    Java 6

---
+++ Comparison operators/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a > b);  // false
+        System.out.println(a < b);  // true
+        System.out.println(b >= 20);// true
+        System.out.println(a <= 10);// true
+    }
+}
